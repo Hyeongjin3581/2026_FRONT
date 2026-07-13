57eb680259448fe8f7b548647903ad63c50d4f02
Hyeongjin3581 <poll3581@naver.com>
2026-07-13 19:00:08 +0900

    2026_07_13_1900

--- Day06/Day06.practice/06practice.js
+++ Day06/Day06.practice/06practice.js
@@ -1,51 +1,76 @@
 // *이 10개이며, 삼각형 그리고 역삼각형 출력. 
 
 // let a = []
-// let html =""
+
 
 
 //일단, 삼각형을 출력하는 방법을 생각해보자. 
 
-// for(let i = 1; i<=10 ; i++){
-//     for(let j=10; j>=1; j--){
-//         if( j > i){
-//             html += "*"
-//         }else{
-//             html+=""
-//         }
-//     }
-//     html +="<br/>"
-// } 
-// document.querySelector("h3").innerHTML = html
 
 
-let html = "";
-let floor = 10;
-let 공백 = "&nbsp&nbsp;"
-// 위쪽 피라미드
-for (let i = 1; i <= floor; i++) {
-    // 공백
-    for (let j = 1; j <= floor - i; j++) {
-        html += 공백;
+let html = " ";
+
+for(let i = 0; i< 10; i++) {
+    for(let j=0 ; j <=i; j++){
+        html+= ' * '
     }
-    // 별
-    for (let j = 1; j <= i * 2 - 1; j++) {
-        html += "*";
-    }
-    html += "<br>";
+    html += '</br>'
 }
 
-// 아래쪽 역피라미드
-for (let i = floor - 1; i >= 1; i--) {
-    // 공백
-    for (let j = 1; j <= floor - i; j++) {
-        html += 공백;
-    }
-    // 별
-    for (let j = 1; j <= i * 2 - 1; j++) {
-        html += "*";
+for(let i = 9; i >= 1; i--){
+    for(let j = 1; j <= i; j++){
+        html += " * ";
     }
     html += "<br>";
 }
 
 document.querySelector("h3").innerHTML = html;
+
+
+
+
+
+// let html = "";
+// let floor = 11;
+// let 공백 = "&nbsp;"
+// // 위쪽 피라미드
+// for (let i = 1; i <= floor; i++) {
+//     // 공백
+//     for (let j = 1; j <= floor - i; j++) {
+//         html += 공백;
+//     }
+//     // 별
+//     for (let j = 1; j <= i -1; j++) {
+//         html += "*";
+//     }
+//     html += "<br>";
+// }
+
+// // 아래쪽 역피라미드
+// for (let i = floor - 1; i >= 1; i--) {
+//     // 공백
+//     for (let j = 1; j <= floor - i; j++) {
+//         html += 공백;
+//     }
+//     // 별
+//     for (let j = 1; j <= i -1; j++) {
+//         html += "*";
+//     }
+//     html += "<br>";
+// }
+
+// let html = " ";
+
+// for(let i = 0; i< 10; i++) {
+//     for(let j=0 ; j <=i; j++){
+//         html+= ' * '
+//     }
+//     html += '</br>'
+// }
+
+// for(let i = 0; i< 10; i++) {
+//     for(let j=1 ; 10 - j; j++){
+//         html+= ' * '
+//     }
+//     html += '</br>'
+// }
