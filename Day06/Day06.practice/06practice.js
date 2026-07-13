9c0003e878ab143f7812179f93ce58c8b28d391d
Hyeongjin3581 <poll3581@naver.com>
2026-07-13 23:07:23 +0900

    2026_07_13_2307

--- Day06/Day06.practice/06practice.js
+++ Day06/Day06.practice/06practice.js
@@ -1,62 +1,5 @@
 // *이 10개이며, 삼각형 그리고 역삼각형 출력. 
 
-// let a = []
-
-
-// let html = " ";
-
-// for(let i = 0; i< 10; i++) {
-//     for(let j=0 ; j <=i; j++){
-//         html+= ' ★ '
-//     }
-//     html += '</br>'
-// }
-
-// for(let i = 9; i >= 1; i--){
-//     for(let j = 1; j <= i; j++){
-//         html += " ★ ";
-//     }
-//     html += "<br>";
-// }
-
-// document.querySelector("h3").innerHTML = html;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 let html=" "
 
@@ -66,13 +9,6 @@ for(let i = 0; i<9; i++){
     }
     html +="</br>"
 }
-
-// for(let i = 10; i<= 1; i--) {
-//     for(let j = 10; j <=i  ; j--){
-//         html +=" ★ "
-//     }
-//     html +="</br>"
-// }
 
 for(let i = 0; i <= 9; i++) {
     for(let j = 9; j >= i; j--) {
